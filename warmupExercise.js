b6c0b8ea3b11bb00feab90fbfd1167a08cc80d65
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-10-09 09:03:23 -0400

    2

--- warmupExercise.js
+++ warmupExercise.js
@@ -1,11 +1,15 @@
-const startNumber = 100;
+const startNumber = 20;
 const endNumber = 3;
 const stepNumber = 7;
 var sum = 0;
 var loopCounter = startNumber;
 
-for (; loopCounter >= 3; loopCounter -= stepNumber) {
-    sum += stepNumber;
+function calculate() {
+    for (; loopCounter >= endNumber; loopCounter -= stepNumber) {
+        sum += stepNumber; // mutating a variable means to change it
+    }
+    return sum;
 }
 
+console.log("sum is ", calculate());
 console.log("sum is ", sum);
